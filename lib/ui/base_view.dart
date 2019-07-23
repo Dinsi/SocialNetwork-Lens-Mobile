f6da9d20bbe94d84c3cb86ef3fb4c050e91d6180
Diogo Agostinho <diogo.agostinho@protonmail.com>
2019-07-23 17:49:50 +0100

    Refactor 3

---
+++ lib/ui/base_view.dart
@@ -0,0 +1,37 @@
+import 'package:aperture/blocs/base_model.dart';
+import 'package:aperture/locator.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BaseView<T extends BaseModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Widget Function(T model) onModelReady;
+
+  const BaseView({Key key, @required this.builder, this.onModelReady})
+      : super(key: key);
+
+  @override
+  _BaseViewState<T> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onModelReady != null) {
+      widget.onModelReady(model);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+      builder: (_) => model,
+      child: Consumer(
+        builder: widget.builder,
+      ),
+    );
+  }
+}
